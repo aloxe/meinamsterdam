c637d4d52085366f8f908780075726d790b08c4c
aloxe <github@blog.re>
2025-07-09 23:07:41 +0200

    add email pour 404 page

---
+++ src/_assets/js/404email.js
@@ -0,0 +1,29 @@
+function makeNotice() {
+  const url = window.location.href;
+  const referer = document.referrer;
+
+  sendEmail("404", "sender@example.com", "\n404 not found on: \n" + url + "\n\n and referer = \n" + referer);
+}
+
+function sendEmail(name, email, message) {
+    const options = {
+        method: 'POST',
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          message: message
+        })
+    };
+
+    var feedback;
+    fetch("/api/envoi.php", options)
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 201) {
+          feedback="📨 message envoyé. 😘 Merci !";
+        } else {
+          feedback="⚠️ Problème: message non envoyé :(";
+        }
+    })
+    .catch(error => console.error("error sendEmail", error))
+}
